d50320c8c3c28d40cd4726a5f802e4189517b2f8
sakutaku <saikalabytova1@gmail.com>
2023-10-21 14:43:21 +0600

    Realized logic, only user and author of photos can see add button

--- front/src/containers/PhotosUser/PhotosUser.tsx
+++ front/src/containers/PhotosUser/PhotosUser.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import './PhotosUser.css';
 import { useAppDispatch, useAppSelector } from '../../app/hook';
 import { selectPhotos, selectPhotosLoading } from '../../store/photosSlice';
@@ -7,12 +7,14 @@ import { fetchPhotos } from '../../store/photosThunk';
 import Spinner from '../../components/Spinner/Spinner';
 import { Fade } from 'react-awesome-reveal';
 import PhotoItem from '../../components/PhotoItem/PhotoItem';
+import { selectUser } from '../../store/usersSlice';
 
 const PhotosUser = () => {
   const { id } = useParams() as { id: string };
   const dispatch = useAppDispatch();
   const photos = useAppSelector(selectPhotos);
   const photosLoading = useAppSelector(selectPhotosLoading);
+  const user = useAppSelector(selectUser);
 
   useEffect(() => {
     if(id) {
@@ -31,13 +33,25 @@ const PhotosUser = () => {
   }
 
   return (
-    <div className="container photos-user-page">
-      {photos.map(photo =>
-        <Fade key={photo._id}>
-          <PhotoItem photo={photo} userPhotos={true}/>
-        </Fade>
-      )
+    <div className="container">
+      {
+        user && user._id === id
+          ?
+          <div className="photos-user-page-wrap">
+            <h2 className="photos-user-page-title">{photos[0].user.displayName}'s gallery</h2>
+            <Link to="/add-photo" className="photos-user-page-btn">Add new photo</Link>
+          </div>
+          :
+          <h2 className="photos-user-page-title">{photos[0].user.displayName}'s gallery</h2>
       }
+      <div className="photos-user-page">
+        {photos.map(photo =>
+          <Fade key={photo._id}>
+            <PhotoItem photo={photo} userPhotos={true}/>
+          </Fade>
+        )
+        }
+      </div>
     </div>
   );
 };
